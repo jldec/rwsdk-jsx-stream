33704083b48288e82775e22196080176b902f830
jldec <jurgen@jldec.me>
2025-09-27 15:50:06 -0400

    starter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# rwsdk-minimal-starter
+# rwsdk-jsx-stream
+
+This is the [RedwoodSDK](https://docs.rwsdk.com/) version of https://github.com/jldec/next-jsx-stream.  
+Based on https://rwsdk-minimal-starter.jldec.workers.dev/
--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,8 +2,11 @@ import { Layout } from './Layout'
 
 export function Home() {
   return (
-    <Layout title="Home">
-      home
+    <Layout title="rwsdk-jsx-stream">
+      <a href="https://github.com/jldec/rwsdk-jsx-stream" className="text-blue-500 underline">
+        GitHub
+      </a>
+
     </Layout>
   )
 }
