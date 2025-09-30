2105da5610ccce79b1a98c1e8a0e39cb0652ba34
jldec <jurgen@jldec.me>
2025-09-30 19:06:05 -0400

    button active state

--- src/app/StreamButton.tsx
+++ src/app/StreamButton.tsx
@@ -11,7 +11,7 @@ export function StreamButton() {
     <div className="mt-4">
       <button
         onClick={async () => setStream(await getStream())}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+        className="bg-blue-500 hover:bg-blue-700 active:translate-y-0.5 text-white font-bold py-2 px-4 rounded-md"
       >
         Get Stream
       </button>
